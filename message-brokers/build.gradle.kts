88d319746247f3cdeb9026ed6eb89f02e1980c1e
alina tarasova <alishka.tarasova@gmail.com>
2024-11-21 10:12:19 +0300

    Added Kafka Benchmarks and report

--- message-brokers/build.gradle.kts
+++ message-brokers/build.gradle.kts
@@ -32,3 +32,39 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+// Задача для сборки JMH тестов
+tasks.register<JavaCompile>("jmhCompile") {
+    source = fileTree("src/main/java")
+    include("**/*.java")
+
+    // Использую зависимости runtimeClasspath
+    classpath = sourceSets.main.get().runtimeClasspath
+
+    // Задание директории
+    destinationDirectory.set(file("build/classes/jmh"))
+
+    // Указываю компилятору использовать аннотации
+    options.compilerArgs = listOf("-proc:only")
+}
+
+
+// Задача для запуска JMH тестов
+tasks.register<JavaExec>("runJmh") {
+    dependsOn("jmhCompile")
+    mainClass.set("org.openjdk.jmh.Main")
+    classpath = files("build/classes/jmh") + sourceSets.main.get().runtimeClasspath
+
+    jvmArgs = listOf(
+        "-Dorg.slf4j.simpleLogger.defaultLogLevel=warn",
+        "-Dorg.slf4j.simpleLogger.log.org.apache.kafka=warn",
+        "-Dorg.slf4j.simpleLogger.log.kafka=warn"
+    )
+
+    args = listOf(
+        "-o", "kafka_benchmark_report.txt",
+        "-f", "1",                            // 1 прогон
+        "-i", "5",                            // 5 итераций измерения
+        "-wi", "2"                            // 2 итерации прогрева
+    )
+}
